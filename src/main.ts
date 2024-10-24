a1ba9d44293196ca4210d5f144de96962a17d0b4
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-10-24 16:08:40 +0300

    done

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-"use strict";
-const getAverage = (array) => {
-    if (array.length === 0)
-        return 0;
-    const sum = array.reduce((acc, el) => acc + el, 0);
+const getAverage = (array : number[])=> {
+    if (array.length === 0 ) return 0;
+    const sum = array.reduce((acc , el)=> acc + el, 0);
     return sum / array.length;
-};
+}
 const array1 = [1, 2, 3, 4, 5];
 const array2 = [4, 5, 9];
-const array3 = [154, 283, 981];
+const array3 = [154,283,981];
 console.log(getAverage(array1));
 console.log(getAverage(array2));
 console.log(getAverage(array3));
