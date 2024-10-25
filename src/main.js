41ff17f8c0be5ac8e5adf27e434c6cc06260613e
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-10-25 22:56:29 +0300

    v1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
 "use strict";
-const hello = 'hello';
-console.log(hello);
+const original = { a: 1, b: { c: 2 }, d: [3, 4] };
+function deepClone(obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    // const clone: object | [] = Array.isArray(obj) ? [] : {};
+    const clone = Array.isArray(obj) ? [] : {};
+    for (let key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            clone[key] = deepClone(obj[key]);
+        }
+    }
+    return clone;
+}
+const copy = deepClone(original);
+console.log(original !== copy);
+// console.log(original.b !== copy.b);
+// console.log(original.d !== copy.d);
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,22 @@
-const hello: string = 'hello';
-console.log(hello);
+let
+
+const original: object = { a: 1, b: { c: 2 }, d: [3, 4] };
+function deepClone(obj: object) {
+    if (obj === null || typeof obj !== 'object') {
+      return obj;
+    }
+    // const clone: object | [] = Array.isArray(obj) ? [] : {};
+    const clone: object = Array.isArray(obj) ? [] : {};
+
+    for (let key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            clone[key] = deepClone((obj as never)[key]);
+        }
+    }
+    return clone;
+}
+
+const copy = deepClone(original);
+console.log(original !== copy);
+// console.log(original.b !== copy.b);
+// console.log(original.d !== copy.d);
