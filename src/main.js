49240370791daa38c57e10fb84a32c4e1bac5a7b
KirillGromyko <goldgrom01@gmail.com>
2024-10-27 17:15:09 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 "use strict";
-const hello = 'hello';
-console.log(hello);
+const arr = [1, 2, 3];
+function reverseArr(arr) {
+    return arr.reverse();
+}
+const reversedArr = reverseArr(arr);
+console.log(arr);
+console.log(reversedArr);
+console.log(reversedArr === arr);
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
-const hello: string = 'hello';
-console.log(hello);
+const arr: number[] = [1,2,3];
+
+function reverseArr(arr: number[]) {
+    return arr.reverse();
+}
+
+const reversedArr = reverseArr(arr)
+console.log(arr);
+console.log(reversedArr);
+console.log(reversedArr === arr);
