96a9efb02e24b835751fde7b4db6bf4054dcdbae
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-10-28 16:15:21 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 "use strict";
 const arr = [1, 2, 3];
+const test = [1, 2, 3, 4, [1, 3], 'hello', { 'Type': 'Script' }];
 function reverseArr(arr) {
-    return arr.reverse();
+    const length = arr.length;
+    for (let i = 0; i < length / 2; i++) {
+        const temp = arr[i];
+        arr[i] = arr[length - 1 - i];
+        arr[length - 1 - i] = temp;
+    }
+    return arr;
 }
 const reversedArr = reverseArr(arr);
-console.log(arr);
+const reversedTest = reverseArr(test);
 console.log(reversedArr);
+console.log(reversedTest);
+console.log(reversedTest === test);
 console.log(reversedArr === arr);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-const arr: number[] = [1,2,3];
+const arr : number[] = [1,2,3]
+const test: (number | number[] | string | object)[] = [1,2,3,4 , [1,3] , 'hello' , {'Type' : 'Script'}];
 
-function reverseArr(arr: number[]) {
-    return arr.reverse();
+function reverseArr<T>(arr: T[]) : T[] {
+    const length = arr.length;
+    for (let i= 0; i < length / 2; i++) {
+        const temp : T = arr[i];
+        arr[i] = arr[length - 1 - i];
+        arr[length - 1 - i] = temp;
+    }
+    return arr;
 }
 
 const reversedArr = reverseArr(arr)
-console.log(arr);
+const reversedTest = reverseArr(test)
 console.log(reversedArr);
+console.log(reversedTest);
+console.log(reversedTest === test);
 console.log(reversedArr === arr);
