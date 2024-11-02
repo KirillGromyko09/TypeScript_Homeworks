716017e3fd0c71ace6cc4c0eec41d7bce707d97f
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-11-02 15:40:06 +0200

    v1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 "use strict";
-const hello = 'hello';
-console.log(hello);
+const user = {
+    city: 'Odessa',
+    street: 'Radistna',
+    zipCode: 65033,
+    name: 'Kirill',
+    age: 21,
+    email: 'hello@gmail.com',
+};
+console.log(user);
+// 2
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,24 @@
-const hello: string = 'hello';
-console.log(hello);
+// 1
+interface Address {
+    city: string;
+    street: string;
+    zipCode: number;
+}
+interface User {
+    name: string;
+    age: number;
+}
+interface UserWithAddress extends Address, User {
+    email: string;
+}
+const user: UserWithAddress = {
+    city: 'Odessa',
+    street: 'Radistna',
+    zipCode: 65033,
+    name: 'Kirill',
+    age: 21,
+    email: 'hello@gmail.com',
+}
+console.log(user);
+
+// 2
