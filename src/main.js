a0da251927b58b968b1fff51cc437c6e414f8670
KirillGromyko <goldgrom01@gmail.com>
2024-11-07 16:46:06 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 "use strict";
-const hello = 'hello';
-console.log(hello);
+function filterByProperty(array, property, value) {
+    return array.filter((item) => item[property] === value);
+}
+const users = [
+    { id: 1, name: 'Alice', age: 25, isActive: true },
+    { id: 2, name: 'Bob', age: 30, isActive: false },
+    { id: 3, name: 'Charlie', age: 35, isActive: true },
+];
+const activeUsers1 = filterByProperty(users, 'isActive', true);
+const activeUsers2 = filterByProperty(users, 'isActive', true);
+console.log(activeUsers1);
+console.log(activeUsers2);
+console.log(activeUsers1 === activeUsers2);
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,22 @@
-const hello: string = 'hello';
-console.log(hello);
+
+function filterByProperty<T , K extends keyof T>(array:T[],property: K, value: T[K]): T[] {
+    return array.filter((item:T) => item[property] === value);
+}
+
+type User = {
+    id: number;
+    name: string;
+    age: number;
+    isActive: boolean;
+};
+
+const users: User[] = [
+    { id: 1, name: 'Alice', age: 25, isActive: true },
+    { id: 2, name: 'Bob', age: 30, isActive: false },
+    { id: 3, name: 'Charlie', age: 35, isActive: true },
+];
+const activeUsers1 = filterByProperty(users, 'isActive', true);
+const activeUsers2 = filterByProperty(users, 'isActive', true);
+console.log(activeUsers1);
+console.log(activeUsers2);
+console.log(activeUsers1 === activeUsers2);
